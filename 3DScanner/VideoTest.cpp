c05aac832d4a218c9d57924d81485dabe60dbf09
Jeff Bishop <bishopjs@plu.edu>
2012-10-30 15:33:14 -0700

    Work on getting intensity for plot

--- 3DScanner/VideoTest.cpp
+++ 3DScanner/VideoTest.cpp
@@ -39,6 +39,25 @@ int main() {
 
 		cvInRangeS(hsvFrame, cvScalar(0, 100, 100), cvScalar(35, 240, 240), redFrame); 
 		cvShowImage("mywindowred", redFrame);
+
+		int redFrameWidth = redFrame->width;
+		int redFrameHeight = redFrame->height;
+
+		int pixelRow = redFrameHeight / 2;
+
+		CvScalar s;
+
+		printf("intensity");
+
+		for (int i = 0; i < redFrameWidth; i++) {
+			s = cvGet2D(hsvFrame,pixelRow,i);
+			printf("%f,", s.val[2]);
+		}
+
+		printf("\n");
+		
+		
+
 		// Do not release the frame!
 		//If ESC key pressed, Key=0x10001B under OpenCV 0.9.7(linux version),
 		//remove higher bits using AND operator
