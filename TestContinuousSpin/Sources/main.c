e692a561cf4f81502a9000ca9b5ab67540268729
scanners <barretgm@plu.edu>
2013-05-11 14:33:29 -0700

    Changing to servo pulse width modulation

--- TestContinuousSpin/Sources/main.c
+++ TestContinuousSpin/Sources/main.c
@@ -18,82 +18,53 @@ void MSDelay(unsigned int itime){
 void interrupt SCI1_ISR(void){
   unsigned char data;
   data = SCI1SR1; /* dummy read to clear flags */
-  data = SCI1DRL;
+  
+  while (SCI1SR1 & 0x20){
+    data = SCI1DRL;
+  }
+  
   isr_flag = 1;
 }
 
 void main(void){
-  const char *stop = "stop";
   unsigned char rc;
-  int i;
   
-  DDRB = 0x0F;      //PORTB0-PORTB3  as output 
-  DDRP = 0x03;      //PORTP0 and PORTP1 as output for 12EN=1 and 34EN=1
-    
-  PORTB=0b00000000; // start with all off
-  PTP=0b00000011;   //Turn on both 12EN and 34EN Enables for 754410 chip
-  
-  PWMPRCLK=0x5; 	//ClockA=Fbus/2**5=24MHz/32=750000 Hz
-	PWMCLK=0x00; 	  //ClockA for Chan 0
-	PWMPOL=0x01; 	  //high and low for polarity
-	PWMCAE=0; 		  //Left aligned
-	PWMCTL=0;		   //8-bit chan and PWM freeze during wait  
-	PWMCNT0=0;		 //start the PWMCount with 0 value
-	PWME=0x01; 	   //Enable PWM chan 0 
+  PWMPRCLK=0x04; //ClockA=Fbus/2**4=24MHz/16=1.5MHz	
+	PWMSCLA=125; 	 //ClockSA=1.5MHz/2x125=6000 Hz
+	PWMCLK=0b00010000; 	 //ClockSA for chan 4
+	PWMPOL=0x10; 		     //high then low for polarity
+	PWMCAE=0x0; 		     //left aligned
+	PWMCTL=0x0;	         //8-bit chan, PWM during freeze and wait
+	PWMPER4=120; 	 //PWM_Freq=ClockSA/120=6000Hz/100=50Hz.
+	PWMCNT4=10;		 //clear initial counter.
   
   SCI1BDL = 0x34;
   SCI1BDH = 0x00;   //set options for SCI communications and interrupts
   
   SCI1CR1 = 0x00;
-  SCI1CR2 = 0x24;
+  SCI1CR2 = 0x2C;
   
   asm("cli");       //enable interrupt globally
   
   rc = SCI1SR1; /* dummy read to clear flags and TDRE */
   SCI1DRH = 0x0000; /* data write to clear TDRE */
+  SCI1DRL = 0x0000; /* data write to clear TDRE */
   
   while(1){
-    if(isr_flag){
-      rc = SCI1SR1; /* dummy read to clear flags */
-      rc = SCI1DRL;
-      MSDelay(20);
-      rc = SCI1DRL;  
+    if(isr_flag){      
+      PWMDTY4=7.5;
+      PWME=0x10; 	   //Enable chan 4 PWM
+      MSDelay(18000);
       
-      for(i = 0; i < 25; i++){
-        PORTB=0b00001010;   
-        MSDelay(50);  
-        PORTB=0b00000110;    
-        MSDelay(50);  
-        PORTB=0b00000101;    
-        MSDelay(50);  
-        PORTB=0b00001001;  
-        MSDelay(50);
-      }
+      while (!(SCI1SR1 & 0x80));  // wait for output buffer empty
+      SCI1DRL = 'S';    //transmit back to signal finished scanning
       
-      //while (*stop != '\0'){
-      //  while (!(SCI1SR1 & 0x80));  // wait for output buffer empty
-      //  SCI1DRL = *stop++;    //transmit back to signal finished scanning
-      //}
+     	PWMDTY4=8; 
+      MSDelay(4600);
       
-      MSDelay(200);
-      PORTB=0b00000000;
-      MSDelay(200);
-      
-      for(i = 0; i < 25; i++){
-        PORTB=0b00001001;  
-        MSDelay(20);
-        PORTB=0b00000101;    
-        MSDelay(20);
-        PORTB=0b00000110;
-        MSDelay(20);
-        PORTB=0b00001010;
-        MSDelay(20);    
-      }
-      
-      PORTB=0b00000000;
+      PWME=0x00; 	   //Enable chan 4 PWM
       isr_flag = 0; 
     }
-    MSDelay(5);
   }
   
   //while (*stop != '\0'){
